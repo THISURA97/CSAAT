8e149a421a676d52c3704ca2b904c1dea5b93fd4
thisura97 <tdinith481@gmail.com>
2021-01-06 21:52:16 +0530

    test2

---
+++ sub-project-1/src/main/java/CSAAT/App copy.java
@@ -0,0 +1,12 @@
+package CSAAT;
+
+public class Appjn{
+    public String getGreeting() {
+        return "Greetings from  sub project 1!";
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new App().getGreeting());
+    }
+}
