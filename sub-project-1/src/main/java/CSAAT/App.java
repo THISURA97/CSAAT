7d7edd294e55a59ae08d46d518512baa7e897fc7
thisura97 <tdinith481@gmail.com>
2021-01-06 21:50:08 +0530

    minor fix

--- sub-project-1/src/main/java/CSAAT/App.java
+++ sub-project-1/src/main/java/CSAAT/App.java
@@ -3,7 +3,7 @@ package CSAAT;
 public class App {
     public String getGreeting() {
         return "Greetings from  sub project 1!";
-        password=123;
+        
     }
 
     public static void main(String[] args) {
